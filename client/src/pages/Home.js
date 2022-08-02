47728b8642f138812212073e11ac26546e81ebd5
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-08-02 16:19:17 -0700

    Add pages/Home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+// import { QUERY_THOUGHTS } from '../utils/queries';
+
+const Home = () => {
+//   const { loading, data } = useQuery(QUERY_RECIPES);
+//   const recipes = data?.recipes || [];
+
+  return (
+    <main>
+        
+    </main>
+  );
+};
+
+export default Home;
