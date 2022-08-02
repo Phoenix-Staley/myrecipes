4e7ce67ae8353d3a9e2bf4bd00ac5cbf7308d127
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-08-01 20:36:18 -0700

    Add Route elements and ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -29,22 +29,40 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Nav />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/:username"
+              element={<Profile />}
+            />
+            <Route
+              path="/me"
+              element={<Profile />}
+            />
+            <Route
+              path="/recipe/:recipeId"
+              element={<Recipe />}
+            />
+            <Route
+              path="/login"
+              element={<loginForm />}
+            />
+            <Route
+              path="/signup"
+              element={<signupForm />}
+            />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
