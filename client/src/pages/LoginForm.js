328a40caf928d2e438da7d17a77c6ab2153995a8
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-08-04 18:16:15 -0700

    Add login form components

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -1,11 +1,86 @@
+import { Button, Form, Input } from "antd";
 import React from "react";
 
-const LoginForm = () => {
+const styles = {
+  form: {
+    marginTop: "25vh"
+  }
+}
+
+const SignupForm = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div>
-        *Login form here*
+    <div className="formHolder">
+      
+      <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      style={styles.form}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <h3 className="formHeader">Login:</h3>
+      <Form.Item
+        label="Email"
+        name="email"
+        hasFeedback
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!",
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          }
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     </div>
   );
 };
 
-export default LoginForm;
+export default SignupForm;
