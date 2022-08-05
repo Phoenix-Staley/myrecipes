eeac7e8609cece1323fb4b078aabbd4e974ef021
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-08-05 11:42:59 -0700

    Hook up LoginForm to LOGIN mutation

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -1,5 +1,8 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Modal } from "antd";
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const styles = {
   form: {
@@ -12,13 +15,36 @@ const SignupForm = () => {
     email: "",
     password: ""
   });
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const [login] = useMutation(LOGIN);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onFinish = async (values) => {
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      showModal();
+    }
   };
 
   const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
+    showModal();
   };
 
   const handleChange = (event) => {
@@ -34,64 +60,67 @@ const SignupForm = () => {
     <div className="formHolder">
       
       <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      style={styles.form}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <h3 className="formHeader">Login:</h3>
-      <Form.Item
-        label="Email"
-        name="email"
-        hasFeedback
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
-      >
-        <Input name="email" onChange={handleChange} />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          }
-        ]}
-      >
-        <Input.Password name="password" onChange={handleChange} />
-      </Form.Item>
-
-      <Form.Item
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
         wrapperCol={{
-          offset: 8,
           span: 16,
         }}
+        initialValues={{
+          remember: true,
+        }}
+        style={styles.form}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <h3 className="formHeader">Login:</h3>
+        <Form.Item
+          label="Email"
+          name="email"
+          hasFeedback
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input name="email" onChange={handleChange} />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            }
+          ]}
+        >
+          <Input.Password name="password" onChange={handleChange} />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <Modal title="Wrong Email or Password" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} className="invalidFormAlert">
+        <p>Please enter a valid email and password, then try again.</p>
+      </Modal>
     </div>
   );
 };
