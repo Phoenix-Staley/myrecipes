e0099800bdab0260a3c3e14969aaa6b0fde1b321
bkhandag <khandagale.b.g@gmail.com>
2022-08-06 12:56:44 -0700

    added recipe queries - all recipies, recipes by id and tags query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,8 +3,11 @@ import { gql } from "@apollo/client";
 export const QUERY_USER = gql`
   {
     user {
+      _id
+      username
       firstName
       lastName
+      email
       savedRecipes {
         _id
         description
@@ -12,8 +15,12 @@ export const QUERY_USER = gql`
         ingredients
         steps
         image
-        creator
-        tags
+        creator {
+          username
+        }
+        tags {
+          name
+        }
       }
       postedRecipes {
         _id
@@ -22,9 +29,79 @@ export const QUERY_USER = gql`
         ingredients
         steps
         image
-        creator
-        tags
+        creator {
+          username
+        }
+        tags {
+          name
+        }
       }
+    }
+  }
+`;
+
+export const QUERY_RECIPES = gql`
+  {
+    recipes {
+      _id
+      description
+      title
+      ingredients
+      steps
+      image
+      creator {
+        username
+      }
+      tags {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_RECIPE = gql`
+  {
+    recipe(recipeId: $recipeId) {
+      _id
+      description
+      title
+      ingredients
+      steps
+      image
+      creator {
+        username
+      }
+      tags {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_TAGGEDRECIPES = gql`
+  {
+    taggedRecipes(tagId: $tagId) {
+      _id
+      description
+      title
+      ingredients
+      steps
+      image
+      creator {
+        username
+      }
+      tags {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_TAGS = gql`
+  {
+    tags {
+      name
+      _id
     }
   }
 `;
