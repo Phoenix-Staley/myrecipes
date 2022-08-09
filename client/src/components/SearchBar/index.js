31900bdeefba4a81a4a6dcb6ed97c29a92aa7a42
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-08-09 09:36:24 -0700

    Add style to the subtitle

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -5,25 +5,31 @@ import { useNavigate } from "react-router-dom";
 const { Option } = AutoComplete;
 
 const styles = {
-  searchPage: {
-    margin: "0 auto",
-    display: "flex",
-    justifyContent: "center",
-    alignContent: "center",
-    marginTop: "15vh"
-  },
-  searchBar: {
-    width: "50%",
-    marginTop: "auto",
-    marginBottom: "auto"
-  },
-  searchBtn: {
-    marginLeft: "1%",
-    backgroundColor: "#ECB365",
-    borderRadius: "6px",
-    border: "white dashed 1px",
-    fontSize: "x-large"
-  }
+    searchPage: {
+        margin: "0 auto",
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        marginTop: "15vh"
+    },
+    searchBar: {
+        width: "50%",
+        marginTop: "auto",
+        marginBottom: "auto"
+    },
+    searchBtn: {
+        marginLeft: "1%",
+        backgroundColor: "#ECB365",
+        borderRadius: "6px",
+        border: "white dashed 1px",
+        fontSize: "larger",
+        cursor: "pointer"
+    },
+    subtitle: {
+        color: "white",
+        width: "100%",
+        textAlign: "center"
+    }
 }
 
 const allTags = ["western", "vegetarian", "vegan", "bland", "mexican", "burger"];
@@ -52,12 +58,12 @@ const Search = () => {
 
   return (
     <>
-        <p style={{color: "white"}}>Recipe tag (all lowercase, I.E. 'vegetarian' or 'burger')</p>
+        
         <div style={styles.searchPage} className="contentHolder">
             <AutoComplete
                 style={styles.searchBar}
                 onChange={handleSearch}
-                placeholder="Recipe tag (all lowercase, I.E. 'vegetarian' or 'burger')"
+                placeholder="Input here"
             >
                 {result.map((suggestion) => (
                     <Option key={suggestion} value={suggestion}>
@@ -65,8 +71,9 @@ const Search = () => {
                     </Option>
                 ))}
             </AutoComplete>
-            <button onClick={loadResults} style={styles.searchBtn}>Search</button>
+            <button className="searchBtn" onClick={loadResults} style={styles.searchBtn}>Search</button>
         </div>
+        <p style={styles.subtitle}>Recipe tag (I.E. 'vegan' or 'breakfast')</p>
     </>
   );
 };
